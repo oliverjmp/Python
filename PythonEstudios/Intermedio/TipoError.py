0a552c573e4236a9b68876da305e9bd98058f13e
Oliver Morales <oliverjmp@gmail.com>
2025-08-30 18:03:32 +0200

    modificacon

--- PythonEstudios/Intermedio/TipoError.py
+++ PythonEstudios/Intermedio/TipoError.py
@@ -17,22 +17,30 @@ import pandas as pd
 
 # 5. Genera un AttributeError accediendo a un atributo que no existe.
 texto = "Hola, mundo"
-print(texto.no_existe)
+#print(texto.no_existe) #Descomentan para error
+print(texto.upper())
 
 # 6. Genera un KeyError al acceder a una clave inexistente de un diccionario.
 
 mi_diccionario = {"nombre": "Oliver", "edad": 30}
-print(mi_diccionario["profesion"])
+# print(mi_diccionario["profesion"]) #Descomentan para error
+print(mi_diccionario["nombre"])
 
 # 7. Genera un TypeError usando tipos incorrectos (índice string en lista).
 mi_lista = [1, 2, 3]
-print(mi_lista["0"])
+#print(mi_lista["0"]) #Descomentan para error
+print(mi_lista[0])
 
 # 8. Genera un ImportError al importar una función que no existe desde un módulo.
-import unicorn_rainbows
-# 9. Genera un ValueError intentando convertir un string no numérico a entero.
-int("no es un número")
+#import unicorn_rainbows  # Descomentan para error
 
+# 9. Genera un ValueError intentando convertir un string no numérico a entero.
+entrada = "manzana"
+try:
+    numero = int(entrada)
+except ValueError:
+    print(f"No se puede convertir '{entrada}' a entero, ValueError")
+    
 # 10. Intenta detectar si un error ocurre usando try-except con un KeyError.
 mi_diccionario = {"clave": "valor"}
 try:
